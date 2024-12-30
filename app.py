4a0a771c95fa6200544510bfa0e02abf95880915
CesarVCify <cesar.vacn@gmail.com>
2024-12-30 17:00:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ if st.button("Limpiar Cantidades Solicitadas"):
     st.success("¡Cantidad solicitada reiniciada a 0 para todos los productos!")
 
 # Botón para actualizar precios
-if st.button("Actualizar Precios desde Catálogo"):
+ if st.button("Actualizar Precios desde Catálogo"):
 python
 Copiar código
     pedidos_df = actualizar_precios(pedidos_df, catalogo_df)
